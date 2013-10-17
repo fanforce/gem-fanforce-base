dfb131d7fe6d6c3532215fb6efce92146fd1193f
Caleb Clark <cclark@mobilizationlabs.com>
2013-10-17 15:37:50 -0400

    fixed include Fanforce::Domains module

--- lib/fanforce/domains.rb
+++ lib/fanforce/domains.rb
@@ -1,66 +1,68 @@
 require_relative 'domain_environments'
 
-class Fanforce::Domains
+module Fanforce::Domains
+  extend Fanforce::Utils
+  def self.included(base) base.extend(self)  end
 
-  def self.base_domain
+  def base_domain
     Fanforce::DomainEnvironments.method(ENV['RACK_ENV'] || 'development').call[:base]
   end
 
-  def self.default_short_domain
+  def default_short_domain
     Fanforce::DomainEnvironments.method(ENV['RACK_ENV'] || 'development').call[:default_short_domain]
   end
 
-  def self.apps_base_domain
+  def apps_base_domain
     Fanforce::DomainEnvironments.method(ENV['RACK_ENV'] || 'development').call[:apps_base]
   end
 
-  def self.plugins_base_domain
+  def plugins_base_domain
     Fanforce::DomainEnvironments.method(ENV['RACK_ENV'] || 'development').call[:plugins_base]
   end
 
-  def self.widgets_base_domain
+  def widgets_base_domain
     Fanforce::DomainEnvironments.method(ENV['RACK_ENV'] || 'development').call[:widgets_base]
   end
 
-  def self.api_domain
+  def api_domain
     'api.' + base_domain
   end
 
-  def self.eye_domain
+  def eye_domain
     'i.' + base_domain
   end
 
-  def self.assets_domain
+  def assets_domain
     'assets.' + base_domain
   end
 
-  def self.controller_domain
+  def controller_domain
     'controller.' + base_domain
   end
 
-  def self.addon_management_url
+  def addon_management_url
     controller_domain + '/db'
   end
 
-  def self.website_domain
+  def website_domain
     'www.' + base_domain
   end
 
-  def self.developers_domain
+  def developers_domain
     'developers.' + base_domain
   end
 
   # these should be moved to edition
 
-  def self.app_domain
+  def app_domain
     'app.' + base_domain
   end
 
-  def self.signup_domain
+  def signup_domain
     'signup.' + base_domain
   end
 
-  def self.marketplace_domain
+  def marketplace_domain
     'marketplace.' + base_domain
   end
 
--- lib/fanforce/version.rb
+++ lib/fanforce/version.rb
@@ -1,3 +1,3 @@
 class Fanforce
-  VERSION = '0.16.0'
+  VERSION = '0.16.1'
 end
