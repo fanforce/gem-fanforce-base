cdbe504b8f096137fe6fa92d7530c69f17373768
Caleb Clark <cclark@mobilizationlabs.com>
2014-11-25 22:38:24 -0500

    changed domain extension for development environment from .gg to .dev

--- lib/fanforce/base/domain_environments.rb
+++ lib/fanforce/base/domain_environments.rb
@@ -3,10 +3,10 @@ module Fanforce::Base::DomainEnvironments
   # Base domains for development environment.
   # @return [hash]
   def self.development
-    root_domain = (ENV['FANFORCE_GEM_TOP_LEVEL_DOMAIN'] || 'gg')
+    root_domain = (ENV['FANFORCE_GEM_TOP_LEVEL_DOMAIN'] || 'dev')
     {
         :base                 => ENV['FANFORCE_GEM_BASE_DOMAIN'] || "fanforce.#{root_domain}",
-        :default_short_domain => ENV['FANFORCE_GEM_DEFAULT_SHORT_DOMAIN'] || "fanforc.#{root_domain}",
+        :default_short_domain => ENV['FANFORCE_GEM_DEFAULT_SHORT_DOMAIN'] || "fnfrc.#{root_domain}",
         :apps_base            => ENV['FANFORCE_GEM_APPS_BASE_DOMAIN']     || "ffapp.#{root_domain}",
     }
   end
@@ -15,9 +15,9 @@ module Fanforce::Base::DomainEnvironments
   # @return [hash]
   def self.test
     {
-      :base                 => 'fanforce.gg',
-      :default_short_domain => 'fanforc.gg',
-      :apps_base            => 'ffapp.gg',
+      :base                 => 'fanforce.dev',
+      :default_short_domain => 'fnfrc.dev',
+      :apps_base            => 'ffapp.dev',
     }
   end
 
@@ -26,7 +26,7 @@ module Fanforce::Base::DomainEnvironments
   def self.staging
     {
         :base                 => 'fanforce-staging.com',
-        :default_short_domain => 'fanforc.us',
+        :default_short_domain => 'fnfrc.us',
         :apps_base            => 'ffapp.us',
     }
   end
